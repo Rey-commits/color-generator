f17fe695f64330a991ffdf505ac0a091f4628289
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-05-27 15:15:02 +0800

    hexValues are now can be copied

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -2,18 +2,32 @@ import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
 const SingleColor = ({rgb, weight, index, hexColor}) => {
+  console.log(hexColor);
   const [alert,setAlert] = useState(false);
   const bcg = rgb.join(",")
 
   const hex = rgbToHex(...rgb)
-  return <article className={`color`} style={{backgroundColor:`rgb(${bcg})`}}>
+  const hexValue = `#${hexColor}`
+  return (
+  <article 
+  className={`color ${index > 5 && "color-light"}`} 
+  style={{backgroundColor:`rgb(${bcg})`}}
+  onClick={() =>{
+    setAlert(true);
+    navigator.clipboard.writeText(hexValue)
+  }}
+  >
   <p className="percent-value">
     {weight}%
   </p>
   <p className="color-value">
-    {hex}
+    {hexValue}
   </p>
+  {alert && <p className="alert">
+    copied to clipboard
+  </p>}
   </article>
+  )
 }
 
 export default SingleColor
