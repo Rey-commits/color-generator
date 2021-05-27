99d803a653bb5c66581f7445ab8d919366232080
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-05-27 15:14:03 +0800

    hexColor value is now displaying

--- src/App.js
+++ src/App.js
@@ -40,8 +40,14 @@ function App() {
         </section>
         <section className="colors">
         {list.map((color,index)=>{
-          console.log(color)
-          return <SingleColor key={index} {...color} index={index}/>
+          
+          return (
+          <SingleColor 
+          key={index} 
+          {...color} 
+          index={index} 
+          hexColor={color.hex} />
+          )
         })}
 
         </section>
